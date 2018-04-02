a049f91f95a90748b0fea49134c1716b54e4989b
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-02 13:38:17 -0600

    prework

---
+++ objects-apis.js
@@ -0,0 +1,17 @@
+/*
+* Objects, Interfaces, and API'S
+* /
+*
+* let dog={
+      name:"Steve"
+      color:"brown"
+      breed:"Pug"
+      size: "Small"
+      bark: function(typeOfBark){
+        console.log("Bark");
+        },
+ };
+
+ dog.bark();
+
+
