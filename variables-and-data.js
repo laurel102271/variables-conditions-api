d891a76b4562b41b3d455c924d00266f22d122df
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-03 08:53:18 -0600

    prework

---
+++ variables-and-data.js
@@ -0,0 +1,12 @@
+/*
+* Variables,Data types, and Typing
+ */
+let wordData;
+
+wordData= "Word List";
+
+console.log(wordData);
+
+let subTotal=100;
+let tax= subTotal*0.07;
+let totalWithTax=tax+subTotal;
